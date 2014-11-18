54e59e2c4ea73829dc7a0395342c94ee250c11ff
jonathangravato <jonathan@gravatodesign.com>
2014-11-18 11:26:07 -0500

    Google analytics and robots

--- includes/head.php
+++ includes/head.php
@@ -50,4 +50,16 @@
     <link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
 
+	<!-- Google analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56569922-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 </head>
